3b380953995e10241785d6c6757ff0ed0d2cc292
Anna Pakkala <anna.pakkala@gmail.com>
2019-03-26 20:16:57 +0200

    Unicafe step 3

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,27 +1,39 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Statistics = (state) => {
+    console.log(state)
+    let yhteensa = state.good + state.bad + state.neutral
+    let keskiarvo = state.good + (state.bad *-1) + (state.neutral*0)/yhteensa
+    return (
+        <div>
+            <h3>Statistiikka</h3>
+        <p>Good: {state.good}</p>
+        <p>Neutral: {state.neutral}</p>
+        <p>Bad: {state.bad}</p>
+        <p>Yhteensä: {yhteensa}</p>
+        <p>Keskiarvo: { keskiarvo}</p>
+        <p>Positiivisia: {state.good/yhteensa}</p>
+        </div>
+    )
+}
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  console.log(bad)
   return (
     <div>
       <h1>Anna palautetta</h1>
       <button onClick={()=>setGood(good+1)}>Good</button>
       <button onClick={()=>setNeutral(neutral+1)}>Neutral</button>
       <button onClick={()=>setBad(bad+1)}>Bad</button>
-      <div>
-          <h3>Statistiikka</h3>
-         <p>Good: {good}</p>
-         <p>Neutral: {neutral}</p>
-         <p>Bad: {bad}</p>
-         <p>Yhteensä: {good+bad+neutral}</p>
-         <p>Keskiarvo: { good + (bad *-1) + (neutral*0)/(good+bad+neutral)}</p>
-         <p>Positiivisia: {good/(good+bad+neutral)}</p>
-      </div>
+    
+      <Statistics bad={bad} good={good} neutral={neutral}/>
+      
     </div>
   )
 }
