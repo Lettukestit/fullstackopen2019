1d0cc671f1d9072312df49b7d191cd443c703a4f
Anna Pakkala <anna.pakkala@gmail.com>
2019-04-02 19:09:45 +0300

    kurssitiedot step8

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -1,64 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import App from './App'
 
-const Header = (props) => {
-    return (
-        <h1>{props.course}</h1>
-    )
-}
-//huom. passing array syntax! {array}
-const Content = ({parts}) => {
-    //console.log({parts})  vs
-    //console.log(parts)      
-    return parts.map(item => (
-         <Part name={item.name} exercises={item.exercises}/>
-    ))
- 
-}
-
-const Total = ({parts}) => {
-    let total = 0;
-    parts.map( item => (
-      total=total + item.exercises
-   ))
-   return <p>yhteensä {total}</p>
-}
-
-const Part = (props) => {
-    return  <p>
-    {props.name} {props.exercises}
-    </p>
-}
-
-
-const App = () => {
-    const course = {
-        name: 'Half Stack -sovelluskehitys',
-        parts: [
-          {
-            name: 'Reactin perusteet',
-            exercises: 10
-          },
-          {
-            name: 'Tiedonvälitys propseilla',
-            exercises: 7
-          },
-          {
-            name: 'Komponenttien tila',
-            exercises: 14
-          }
-        ]
-      }
-
-  return (
-    <div>
-    <Header course={course.name} />
-    <Content parts={course.parts}/>
-    <Total parts={course.parts}/>
-    </div>
-  )
-
-}
 
 
 ReactDOM.render(<App />, document.getElementById('root'))
