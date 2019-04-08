33d63f0f39aec1c406ce2cd11c99d8627d7619a9
Anna Pakkala <anna.pakkala@gmail.com>
2019-04-08 21:32:57 +0300

    puhelinluettelo step4

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -12,13 +12,17 @@ const Numerot = ({persons}) => {
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: "040-12345678" }
+    { name: 'Arto Hellas', number: "040-12345678" },
+    { name: 'Anna Pakkala', number: "040-123454568" },
+    { name: 'Risto Reipas', number: "040-12225678" },
+    { name: 'Ville Valo', number: "040-123453338" }
   ]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber] = useState('')
 
   const addEntry = (event) => {
     event.preventDefault()
+
     //tarkista onko tämä nimi jo listassa
     let exists = false;
     persons.forEach((item) => {
@@ -26,11 +30,11 @@ const App = () => {
         exists = true
       }
     })
+
     if(exists) {
       alert(`${newName} on jo luettelossa`)
     } else {
       let copy = [...persons, {name:newName, number:newNumber}] 
-      console.log("copy:",copy)
       setPersons(copy)
     }
     
@@ -38,17 +42,27 @@ const App = () => {
   }
 
   const handleNameChange = (event)=> {
-    console.log("handleNameChange event target value:",event.target.value)
     setNewName(event.target.value)
   }
   const handleNumberChange = (event)=> {
-    console.log("handleNameChange event target value:",event.target.value)
     setNewNumber(event.target.value)
+  }
+  const search = (event) => {
+    
+    let copy = persons.filter((item) => {
+      //compare in upper case = case insensitive
+       return  item.name.toUpperCase().indexOf (event.target.value.toUpperCase()) > -1
+    })
+    setPersons(copy)
   }
 
   return (
     <div>
       <h2>Puhelinluettelo</h2>
+      <label>Haku
+      <input onChange={search}/>
+      </label>
+      <h3>Lisää numero</h3>
       <form onSubmit={addEntry}>
         <div>
           nimi: <input value={newName} onChange={handleNameChange}/> <br/>
